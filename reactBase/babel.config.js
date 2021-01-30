ea453901da1ae08b7d9d56ad59633d809cc472e7
rodneyems <rodney.martins@live.com>
2021-01-29 22:43:21 -0300

    Configs

---
+++ reactBase/babel.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+    presets: [
+        '@babel/preset-env',
+        '@babel/preset-react'
+    ],
+    plugins: [
+        "@babel/plugin-transform-runtime",
+        [
+            "@babel/plugin-transform-spread",
+            {
+              "loose": false
+            }
+        ]
+    ]
+}
