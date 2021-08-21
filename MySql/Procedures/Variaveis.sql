1c7badceebd495c9c135683dc2a14640e50f32a6
rodneyems <rodney.martins@live.com>
2021-08-21 15:51:51 -0300

    update var

--- MySql/Procedures/Variaveis.sql
+++ MySql/Procedures/Variaveis.sql
@@ -1,6 +1,8 @@
 CREATE PROCEDURE `variavel` ()
 BEGIN
+
 declare minha_variavel char(12) default 'SOU EU SUA VARIAVEL';
 declare meu_timestamp timestamp default timestamp();
 select minha_variavel as VAR, meu_timestamp as meu_ts;
+
 END
