6215686676fda813351765558b7e6fd5141ce592
rodneyems <rodney.martins@live.com>
2021-11-03 09:22:27 -0300

    import data

--- MySql/Avançado/Importando Dados CSV via LOAD DATA.sql
+++ MySql/Avançado/Importando Dados CSV via LOAD DATA.sql
@@ -1,9 +1,20 @@
 SELECT * FROM reno.estados;
+SELECT * FROM reno.irr where id_lat = 1802;
+
+
+
 SET SQL_SAFE_UPDATES = 0;
 SHOW VARIABLES LIKE "secure_file_priv";
 LOAD DATA 
 INFILE 'C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/cidades.CSV'
 INTO TABLE estados
+CHARACTER SET utf8
+FIELDS TERMINATED BY ';' 
+IGNORE 1 LINES;
+
+LOAD DATA 
+INFILE 'C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/tab_irr.csv'
+INTO TABLE irr
 CHARACTER SET utf8
 FIELDS TERMINATED BY ';' 
 IGNORE 1 LINES;
