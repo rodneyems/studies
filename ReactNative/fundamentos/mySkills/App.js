6d3b8fd9dd699f0561d3108ef0d16d5347eb77ab
rodneyems <rodney.martins@live.com>
2022-02-10 12:09:56 -0300

    add statusbar

--- ReactNative/fundamentos/mySkills/App.js
+++ ReactNative/fundamentos/mySkills/App.js
@@ -1,6 +1,12 @@
 import React from 'react';
+import {StatusBar} from 'react-native';
 import Home from './src/Home';
 
 export default function App() {
-  return <Home />;
+  return (
+    <>
+      <StatusBar barStyle="dark-content" />
+      <Home />
+    </>
+  );
 }
