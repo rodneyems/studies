480a708ff031c6d4ea5f26be70f9bdbb24c0a951
rodneyems <rodney.martins@live.com>
2022-02-24 12:27:58 -0300

    add transactionstype

--- ReactNative/gofinace/src/screens/Register/index.tsx
+++ ReactNative/gofinace/src/screens/Register/index.tsx
@@ -1,12 +1,15 @@
 import React from "react";
 import { Button } from "../../componets/Form/Button";
+import { CategorySelect } from "../../componets/Form/CategorySelect";
 import { Input } from "../../componets/Form/Input";
+import { TransactionTypeButton } from "../../componets/Form/TransactionTypeButton";
 import { 
   Container, 
   Header,
   Title,
   Form,
   Fields,
+  TransactionsTypes,
 } from "./styles";
 
 export function Register(){
@@ -19,6 +22,11 @@ export function Register(){
         <Fields>
           <Input placeholder="Nome"/>
           <Input placeholder="Preço"/>
+          <TransactionsTypes>
+            <TransactionTypeButton type="up" title="Income"/>
+            <TransactionTypeButton type="down" title="Outcome"/>
+          </TransactionsTypes>
+            <CategorySelect title={"Categoria"}></CategorySelect>
         </Fields>
 
         <Button title="Confirmar"></Button>
