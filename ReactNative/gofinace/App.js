4218d5071cb9e55924bdd0f497a93cb42401ae8c
rodneyems <rodney.martins@live.com>
2022-03-03 08:36:19 -0300

    add tab navigation

--- ReactNative/gofinace/App.js
+++ ReactNative/gofinace/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
 import { ThemeProvider } from 'styled-components/native';
-import { Dasboard } from './src/screens/Dashboard/Dashboard';
 import theme from './src/global/styles/theme';
 import { 
   useFonts,
@@ -9,6 +9,9 @@ import {
   Poppins_700Bold
 } from '@expo-google-fonts/poppins'; 
 import AppLoading from 'expo-app-loading';
+import { CategorySelect } from './src/screens/CategorySelect';
+import { Register } from './src/screens/Register';
+import { AppRoutes } from './src/routes/app.routes';
 export default function App() {
   const [fontsLoaded] = useFonts ({
     Poppins_400Regular,
@@ -20,7 +23,9 @@ export default function App() {
   }
   return (
     <ThemeProvider theme={theme}>
-      <Dasboard></Dasboard>
+      <NavigationContainer>
+        <AppRoutes/>
+      </NavigationContainer>
     </ThemeProvider>
   );
 }
---
+++ ReactNative/gofinace/src/routes/app.routes.tsx
@@ -0,0 +1,60 @@
+import React from 'react';
+import { MaterialIcons } from '@expo/vector-icons';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { Dashboard } from '../screens/Dashboard/Dashboard';
+import { Register } from '../screens/Register';
+import theme from '../global/styles/theme';
+
+const { Navigator, Screen } = createBottomTabNavigator();
+
+export function AppRoutes() {
+  return (
+    <Navigator
+      screenOptions={{
+        tabBarActiveTintColor: theme.colors.secondary,
+        tabBarInactiveTintColor: theme.colors.text,
+        tabBarLabelPosition: 'beside-icon',
+      }}
+    >
+      <Screen
+        name='Listagem'
+        component={Dashboard}
+        options={{
+          tabBarIcon: ({ size, color }) => (
+            <MaterialIcons
+              name='format-list-bulleted'
+              size={size}
+              color={color}
+            />
+          ),
+        }}
+      />
+      <Screen
+        name='Cadastrar'
+        component={Register}
+        options={{
+          tabBarIcon: ({ size, color }) => (
+            <MaterialIcons
+              name='attach-money'
+              size={size}
+              color={color}
+            />
+          ),
+        }}
+      />
+      <Screen
+        name='Resumo'
+        component={Register}
+        options={{
+          tabBarIcon: ({ size, color }) => (
+            <MaterialIcons
+              name='pie-chart'
+              size={size}
+              color={color}
+            />
+          ),
+        }}
+      />
+    </Navigator>
+  );
+}
