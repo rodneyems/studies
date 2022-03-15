d6e9b87ccb5d8da4005eefd968c8b9f33bb23f7f
rodneyems <rodney.martins@live.com>
2022-03-15 11:04:31 -0300

    add loading and dynamic date

--- ReactNative/gofinace/src/screens/Dashboard/index.tsx
+++ ReactNative/gofinace/src/screens/Dashboard/index.tsx
@@ -1,6 +1,8 @@
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import React, { useCallback, useEffect, useState } from 'react';
 import { HighLightCard } from '../../componets/HighLightCard';
+import { ActivityIndicator } from 'react-native';
+import { useTheme } from 'styled-components';
 import 'intl';
 import 'intl/locale-data/jsonp/pt-BR';
 import {
@@ -22,6 +24,7 @@ import {
   Title,
   TransactionsList,
   LogoutButton,
+  LoadContainer,
 } from './styles';
 import { useFocusEffect } from '@react-navigation/native';
 
@@ -29,14 +32,52 @@ export interface DataListProps extends TransactionProps {
   id: string;
 }
 
+interface HighLightCardData {
+  entries: { amount: string, lastTransaction: string; };
+  expensives: { amount: string, lastTransaction: string };
+  total: { amount: string; };
+}
+
 export function Dashboard() {
+  const [isLoading, setIsLoading] = useState(true);
   const [data, setData] = useState<DataListProps[]>([]);
+  const [highLightCard, setHighLightCard] = useState<HighLightCardData>({
+    entries: { amount: '0' },
+    expensives: { amount: '0' },
+    total: { amount: '0' },
+  } as HighLightCardData);
+  const theme = useTheme();
+  function getLastTransactionDate(
+    collection: DataListProps[],
+    type: 'positive' | 'negative'
+  ) {
+    const lastTransaction = Math.max.apply(
+      Math,
+      collection
+        .filter((transaction) => transaction.type === type)
+        .map((transaction) => new Date(transaction.date).getTime())
+    );
+    const lastTransactionFormatted = Intl.DateTimeFormat('pt-BR', {
+      day: '2-digit',
+      month: '2-digit',
+      year: '2-digit',
+    }).format(new Date(lastTransaction));
+
+    return lastTransactionFormatted;
+  }
   async function loadTransactions() {
+    let entriesSum = 0;
+    let expensiveSum = 0;
     const dataKey = '@gofinace:transactions';
     const response = await AsyncStorage.getItem(dataKey);
     const transactions = response ? JSON.parse(response) : [];
     const transactionsFormatted: DataListProps[] = transactions.map(
       (item: DataListProps) => {
+        if (item.type === 'positive') {
+          entriesSum += Number(item.amount);
+        } else {
+          expensiveSum += Number(item.amount);
+        }
         const amount = Number(item.amount).toLocaleString('pt-BR', {
           style: 'currency',
           currency: 'BRL',
@@ -58,67 +99,103 @@ export function Dashboard() {
       }
     );
     setData(transactionsFormatted);
+    const lastTransactionEntries = getLastTransactionDate(transactions, 'positive');
+    const lastTransactionExpensives = getLastTransactionDate(transactions, 'negative');
+
+    setHighLightCard({
+      entries: {
+        amount: entriesSum.toLocaleString('pt-BR', {
+          style: 'currency',
+          currency: 'BRL',
+        }),
+        lastTransaction: lastTransactionEntries
+      },
+      expensives: {
+        amount: expensiveSum.toLocaleString('pt-BR', {
+          style: 'currency',
+          currency: 'BRL',
+        }),
+        lastTransaction: lastTransactionExpensives
+      },
+      total: {
+        amount: (entriesSum - expensiveSum).toLocaleString('pt-BR', {
+          style: 'currency',
+          currency: 'BRL',
+        }),
+      },
+    });
+    setIsLoading(false);
   }
 
   useEffect(() => {
     loadTransactions();
   }, []);
 
-  useFocusEffect(useCallback(()=>{
-    loadTransactions()
-  }, []))
-  
+  useFocusEffect(
+    useCallback(() => {
+      loadTransactions();
+    }, [])
+  );
+
   return (
     <Container>
-      <Header>
-        <UserWrapper>
-          <UserInfo>
-            <Photo source={{ uri: 'https://bit.ly/3oJ3okZ' }} />
-            <User>
-              <Greattings>Olá, </Greattings>
-              <UserName>Rodney</UserName>
-            </User>
-          </UserInfo>
-          <LogoutButton onPress={() => {}}>
-            <PowerIcon name='power' />
-          </LogoutButton>
-        </UserWrapper>
-      </Header>
-      <HighLightCards>
-        <HighLightCard
-          data={{
-            type: 'up',
-            title: 'Entrada',
-            amount: 'R$ 50.000,00',
-            lastTransaction: 'Última transação realizada dia 15/02/2022',
-          }}
-        />
-        <HighLightCard
-          data={{
-            type: 'down',
-            title: 'Saída',
-            amount: 'R$ 50.000,00',
-            lastTransaction: 'Última transação realizada dia 20/02/2022',
-          }}
-        />
-        <HighLightCard
-          data={{
-            type: 'total',
-            title: 'Resumo',
-            amount: 'R$ 50.000,00',
-            lastTransaction: 'Última transação realizada dia 28/02/2022',
-          }}
-        />
-      </HighLightCards>
-      <Transactions>
-        <Title>Listagem</Title>
-        <TransactionsList
-          data={data}
-          renderItem={({ item }) => (
-            <TransactionCard data={item}></TransactionCard>
-          )}
-        />
-      </Transactions>
+      {isLoading ? (
+        <LoadContainer>
+          <ActivityIndicator color={theme.colors.primary} size={'large'} />
+        </LoadContainer>
+      ) : (
+        <>
+          <Header>
+            <UserWrapper>
+              <UserInfo>
+                <Photo source={{ uri: 'https://bit.ly/3oJ3okZ' }} />
+                <User>
+                  <Greattings>Olá, </Greattings>
+                  <UserName>Rodney</UserName>
+                </User>
+              </UserInfo>
+              <LogoutButton onPress={() => {}}>
+                <PowerIcon name='power' />
+              </LogoutButton>
+            </UserWrapper>
+          </Header>
+          <HighLightCards>
+            <HighLightCard
+              data={{
+                type: 'up',
+                title: 'Entrada',
+                amount: highLightCard.entries.amount,
+                lastTransaction: 'Última entrada realizada ' + highLightCard.entries.lastTransaction,
+              }}
+            />
+            <HighLightCard
+              data={{
+                type: 'down',
+                title: 'Saída',
+                amount: highLightCard.expensives.amount,
+                lastTransaction: 'Última saida realizada ' + highLightCard.expensives.lastTransaction,
+              }}
+            />
+            <HighLightCard
+              data={{
+                type: 'total',
+                title: 'Resumo',
+                amount: highLightCard.total.amount,
+                lastTransaction: 'Última transação realizada dia 28/02/2022',
+              }}
+            />
+          </HighLightCards>
+          <Transactions>
+            <Title>Listagem</Title>
+            <TransactionsList
+              data={data}
+              renderItem={({ item }) => (
+                <TransactionCard data={item}></TransactionCard>
+              )}
+            />
+          </Transactions>
+        </>
+      )}
     </Container>
   );
 }
--- ReactNative/gofinace/src/screens/Dashboard/styles.ts
+++ ReactNative/gofinace/src/screens/Dashboard/styles.ts
@@ -1,20 +1,19 @@
-import styled from 'styled-components/native'
-import { RFPercentage, RFValue } from 'react-native-responsive-fontsize'
+import styled from 'styled-components/native';
+import { RFPercentage, RFValue } from 'react-native-responsive-fontsize';
 import { Feather } from '@expo/vector-icons';
 import { FlatList } from 'react-native';
-import { BorderlessButton } from 'react-native-gesture-handler'
-
+import { BorderlessButton } from 'react-native-gesture-handler';
 
 export const Container = styled.View`
-    flex: 1;
-    background-color: ${({theme}) => theme.colors.background };
+  flex: 1;
+  background-color: ${({ theme }) => theme.colors.background};
 `;
 
 export const Header = styled.View`
   width: 100%;
   align-items: center;
   height: ${RFPercentage(40)}px;
-  background-color: ${({theme}) => theme.colors.primary};  
+  background-color: ${({ theme }) => theme.colors.primary};
 `;
 
 export const UserWrapper = styled.View`
@@ -24,48 +23,46 @@ export const UserWrapper = styled.View`
   justify-content: space-between;
   width: 100%;
   padding: 0 24px;
-`
+`;
 
 export const UserInfo = styled.View`
   flex-direction: row;
   align-items: center;
-`
+`;
 
 export const Photo = styled.Image`
   width: ${RFValue(48)}px;
   height: ${RFValue(48)}px;
   border-radius: 8px;
-`
+`;
 
 export const User = styled.View`
   margin-left: 17px;
-`
+`;
 
 export const Greattings = styled.Text`
-  color: ${({theme}) => theme.colors.shape};
-  font-family: ${({theme}) => theme.fonts.regular};
+  color: ${({ theme }) => theme.colors.shape};
+  font-family: ${({ theme }) => theme.fonts.regular};
   font-size: ${RFValue(18)}px;
   margin-bottom: -8px;
-`
+`;
 
 export const UserName = styled.Text`
-  color: ${({theme}) => theme.colors.shape};
-  font-family: ${({theme}) => theme.fonts.bold};
+  color: ${({ theme }) => theme.colors.shape};
+  font-family: ${({ theme }) => theme.fonts.bold};
   font-size: ${RFValue(18)}px;
-`
-export const LogoutButton = styled(BorderlessButton)`
-
-`
+`;
+export const LogoutButton = styled(BorderlessButton)``;
 
 export const PowerIcon = styled(Feather)`
-  color: ${({theme}) => theme.colors.secondary};
+  color: ${({ theme }) => theme.colors.secondary};
   font-size: ${RFValue(24)}px;
-`
+`;
 
 export const HighLightCards = styled.ScrollView.attrs({
   horizontal: true,
   showsHorizontalScrollIndicator: false,
-  contentContainerStyle: {paddingHorizontal: 24}
+  contentContainerStyle: { paddingHorizontal: 24 },
 })`
   width: 100%;
   position: absolute;
@@ -78,15 +75,20 @@ export const Transactions = styled.View`
   padding: 0 24px;
   margin-top: ${RFPercentage(12)}px;
   align-items: flex-start;
-`
+`;
 
 export const Title = styled.Text`
-  font-family: ${({theme}) => theme.fonts.medium};
+  font-family: ${({ theme }) => theme.fonts.medium};
   font-size: ${RFValue(18)}px;
-
-`
+`;
 export const TransactionsList = styled.FlatList.attrs({
   showsVerticalScrollIndicator: false,
 })`
   width: 100%;
 ` as unknown as typeof FlatList;
+
+export const LoadContainer = styled.View`
+  flex: 1;
+  align-items: center;
+  justify-content: center;
+`;
