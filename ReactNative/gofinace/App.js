e5b846f8d4da3a94a06826c522ef405cc5906ee2
rodneyems <rodney.martins@live.com>
2022-03-22 13:31:40 -0300

    refactor statusbar color

--- ReactNative/gofinace/App.js
+++ ReactNative/gofinace/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { ThemeProvider } from 'styled-components/native';
 import theme from './src/global/styles/theme';
+import { StatusBar } from 'react-native';
 import { 
   useFonts,
   Poppins_400Regular,
@@ -9,8 +10,6 @@ import {
   Poppins_700Bold
 } from '@expo-google-fonts/poppins'; 
 import AppLoading from 'expo-app-loading';
-import { CategorySelect } from './src/screens/CategorySelect';
-import { Register } from './src/screens/Register';
 import { AppRoutes } from './src/routes/app.routes';
 export default function App() {
   const [fontsLoaded] = useFonts ({
@@ -24,6 +23,7 @@ export default function App() {
   return (
     <ThemeProvider theme={theme}>
       <NavigationContainer>
+        <StatusBar barStyle="light-content" />
         <AppRoutes/>
       </NavigationContainer>
     </ThemeProvider>
